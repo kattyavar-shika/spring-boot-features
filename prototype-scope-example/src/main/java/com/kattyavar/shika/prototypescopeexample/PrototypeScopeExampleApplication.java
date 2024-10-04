3d0373548d21b2add71e45bf21be20da30092359
kattyavar-shika <kattyavar.shika@gmail.com>
2024-10-04 18:46:03 +0530

    init.

---
+++ prototype-scope-example/src/main/java/com/kattyavar/shika/prototypescopeexample/PrototypeScopeExampleApplication.java
@@ -0,0 +1,13 @@
+package com.kattyavar.shika.prototypescopeexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeScopeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeScopeExampleApplication.class, args);
+	}
+
+}
---
+++ prototype-scope-example/src/test/java/com/kattyavar/shika/prototypescopeexample/PrototypeScopeExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kattyavar.shika.prototypescopeexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeScopeExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
