92a1448afb4c4563977ba83ddc32b5e5859ad3be
kattyavar-shika <kattyavar.shika@gmail.com>
2024-11-19 10:10:33 +0530

    added UserServiceDetails Bean for in Memory users.

--- basic.security/src/main/java/com/kattyavar/shika/basic/security/config/SecurityConfig.java
+++ basic.security/src/main/java/com/kattyavar/shika/basic/security/config/SecurityConfig.java
@@ -6,7 +6,16 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
@@ -27,4 +36,29 @@ public class SecurityConfig {
       .build();
 
   }
+
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    UserDetails userDetails1 = User
+      .withDefaultPasswordEncoder()
+      .username("temp1")
+      .password("test1pass")
+      .roles("USER")
+      .build();
+
+    UserDetails userDetails2 = User
+      .withDefaultPasswordEncoder()
+      .username("temp2")
+      .password("test2pass")
+      .roles("USER")
+      .build();
+
+
+    InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager(userDetails1, userDetails2);
+
+    return inMemoryUserDetailsManager;
+
+  }
+
 }
